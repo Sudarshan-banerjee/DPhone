7e9ee5ce51c7a7572da4c7a86ef5d79163dc6f09
Sudarshan-banerjee <93525443+Sudarshan-banerjee@users.noreply.github.com>
2023-02-20 10:33:39 +0530

    Add files via upload

---
+++ Api-Gateway/src/main/java/com/dphone/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.dphone.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiGatewayApplication.class, args);
+		System.out.println("chal gya ab party karenge!");
+	}
+
+}
